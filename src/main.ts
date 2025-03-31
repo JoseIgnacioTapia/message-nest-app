e89e32559e2ceec711ae81705a07dd1e82f2d302
JoseIgnacioTapia <josignaciot@gmail.com>
2025-03-31 11:23:12 -0300

    Add validation rules

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { MessagesModule } from './messages/messages.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
+
+  // Habilita el validaro global
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
